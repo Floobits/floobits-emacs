203b1eb4b81a6dda91b07f3de04db8004343c842
Matt Kaniaris <mkaniaris@gmail.com>
2012-11-08 23:52:03 -0800

    no prints out text if it would be changed... not terribly helpful)

--- floobits.el
+++ floobits.el
@@ -1,13 +1,17 @@
 (defun test-func (begin end)
   "does stuff"
-  (message "%s:%s" begin end)
+  (print (buffer-substring-no-properties begin end))
 )
-
 
 (add-hook 'before-change-functions
 	  'test-func
 	  nil
 	  t)
+
+
+
+
+
 
 
 
