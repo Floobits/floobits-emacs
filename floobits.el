ce4099028d7e7553b0016ba743b11403906165c0
Matt Kaniaris <mkaniaris@gmail.com>
2012-11-12 21:34:00 -0800

    now showing changed text

--- floobits.el
+++ floobits.el
@@ -1,19 +1,15 @@
 
-(defun change-func (var begin end &rest)
+(defun change-func (var begin end old_length)
   "does stuff"
   (setq contents (buffer-substring-no-properties begin end))
   (set var
 	(mapcar 'symbol-value '(begin end contents))
 	)
-  (print before-change)
-  (print after-change)
+  (print (symbol-value var))
   )
 
-(add-hook 'before-change-functions
-	  (apply-partially 'change-func 'before-change)
-	  nil
-	  t)
 (add-hook 'after-change-functions
 	  (apply-partially 'change-func 'after-change)
 	  nil
 	  t)
+
