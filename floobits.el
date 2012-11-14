95aa9bfbb57bdb1c31b823d7d44c82934a1fc1b2
Matt Kaniaris <mkaniaris@gmail.com>
2012-11-13 20:25:19 -0800

    now with nonworking network communication

--- floobits.el
+++ floobits.el
@@ -1,5 +1,30 @@
+(defvar floobits-agent-port 9999
+    "port of the floobits agent")
 
-(defun change-func (var begin end old_length)
+(defvar floobits-agent-host "127.0.0.1"
+    "host of the floobits agent")
+
+(defun connect-to-agent nil
+    "connects to the agent"
+    (interactive)
+    (make-network-process :name "floobits" :buffer "*floobits*" :family 'ipv4 :host connect-to-agent :service floobits-agent-port :sentinel 'floobits-sentinel :filter 'floobits-filter ) 
+    )
+
+(defun floobits-sentinel (proc msg)
+  (message msg proc)
+  )
+
+(defun floobits-filter (proc string)   
+  (message string)
+  )
+
+(defun floobits-stop nil
+  "stop an emacs tcp listener"
+  (interactive)
+  (delete-process "listen")
+  )
+
+(defun change-func (var begin end &optional &rest old_length)
   "does stuff"
   (setq contents (buffer-substring-no-properties begin end))
   (set var
@@ -7,6 +32,11 @@
 	)
   (print (append (symbol-value var) old_length))
   )
+
+(add-hook 'before-change-functions
+	  (apply-partially 'change-func 'before-change)
+	  nil
+	  t)
 
 (add-hook 'after-change-functions
 	  (apply-partially 'change-func 'after-change)
