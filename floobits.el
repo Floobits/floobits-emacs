9f81774a9d263227a2928757967c265360ad9b40
Matt Kaniaris <mkaniaris@gmail.com>
2012-11-25 14:53:38 -0800

    no longer using broken tcp wrapper.

--- floobits.el
+++ floobits.el
@@ -1,56 +1,31 @@
-(require 'tcp-client)
+(require 'json)
 
-(defun floobits-error-report(buffer server port error)
-  (save-excursion
-    (set-buffer buffer)
-    (insert (format "[error] %s:%i -- %s\n" server port error)))
-  )
-     
-(defun floobits-connection-report(buffer server port)
-  (save-excursion
-    (set-buffer buffer)
-    (insert (format "[connect] %s:%i -- Connection established\n" server port))))
+(setq floobits-change-set '((before . '(-1 -1 "")) (after . '(-1 -1 ""))))
 
-(defun floobits-abort-report(buffer server port)
-  (save-excursion
-    (set-buffer buffer)
-    (insert (format "[error] %s:%i -- Abort connection\n" server port))))
 
-(defun floobits-sentinel-report(process event)
-  (save-excursion
-    (set-buffer (process-buffer process))
-    (insert (format "[event] Process: %s had the event -- %s" process event))))
+(defun floobits-listener(process response)
+  (print response))
 
-(defun floobits-filter-report(process message)
-  (save-excursion
-    (set-buffer (process-buffer process))
-    (insert (format "[got] %s" message))))
-     
-(tcp-connect "*floobits*" 
-	     (make-new-tcp-connection :server "127.0.0.1" :port 4567)
-	     (make-new-tcp-hooks 
-	      :connection-failed-handler 'floobits-error-report
-	      :connection-established-handler 'floobits-connection-report
-	      :connection-abort-handler 'floobits-abort-report
-	      :sentinel-handler 'floobits-sentinel-report
-	      :filter-handler 'floobits-filter-report)
-	     )
+(defun create-connection()
+  (setq floo (open-network-stream "floobits" nil "localhost" 4567))
+  (set-process-coding-system pserv 'utf-8 'utf-8)
+  (set-process-filter floo 'floobits-listener))
 
 (defun change-func (var begin end &optional &rest old_length)
   "does stuff"
   (setq contents (buffer-substring-no-properties begin end))
   (set var
-	(mapcar 'symbol-value '(begin end contents))
-	)
-  (print (append (symbol-value var) old_length))
-  )
+	(mapcar 'symbol-value '(begin end contents old_length))))
 
-(add-hook 'before-change-functions
-	  (apply-partially 'change-func 'before-change)
-	  nil
-	  t)
-
+(add-hook 'before-change-functions 
+	   (lambda (begin end) (put 'floobits-change-set 'before '(begin end (buffer-substring-no-properties begin end))) (message (symbol-plist 'floobits-change-set )))
+	   nil
+	   t)
+	  
 (add-hook 'after-change-functions
-	  (apply-partially 'change-func 'after-change)
+	  (lambda (begin end old_length) (setq floobits-after-change (buffer-substring-no-properties begin end))
+	    (process-send-string floo (json-encode floobits-change-set)))
 	  nil
 	  t)
+
+(create-connection)
