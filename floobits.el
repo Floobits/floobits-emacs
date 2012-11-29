36caf8901220f6c290eacc58fe44454017450297
Matt Kaniaris <mkaniaris@gmail.com>
2012-11-28 20:14:35 -0800

    now setting the changes and shipping them off the wire

--- floobits.el
+++ floobits.el
@@ -1,30 +1,42 @@
 (require 'json)
 
-(setq floobits-change-set '((before . '(-1 -1 "")) (after . '(-1 -1 ""))))
-
+(setq floobits-change-set ())
 
 (defun floobits-listener(process response)
   (print response))
 
 (defun create-connection()
   (setq floo (open-network-stream "floobits" nil "localhost" 4567))
-  (set-process-coding-system pserv 'utf-8 'utf-8)
+  (set-process-coding-system floo 'utf-8 'utf-8)
   (set-process-filter floo 'floobits-listener))
 
 (defun change-func (var begin end &optional &rest old_length)
   "does stuff"
   (setq contents (buffer-substring-no-properties begin end))
   (set var
-	(mapcar 'symbol-value '(begin end contents old_length))))
+       (mapcar 'symbol-value '(begin end contents old_length))))
+
+(defun get-text (begin end)
+  (buffer-substring-no-properties begin end))
+
+(defun before-change (begin end)
+  (let ((text (get-text begin end)))
+    (add-to-list 'floobits-change-set (cons 'before `(,begin ,end ,text)))))
+
+(defun after-change (begin end old_length)
+  (let ((text (get-text begin end)))
+    (add-to-list 'floobits-change-set (cons 'after `(,begin ,end ,text)))
+    (process-send-string floo (json-encode 'floobits-change-set)))
+  (print floobits-change-set)
+  (setq floobits-change-set nil))
 
 (add-hook 'before-change-functions 
-	   (lambda (begin end) (put 'floobits-change-set 'before '(begin end (buffer-substring-no-properties begin end))) (message (symbol-plist 'floobits-change-set )))
+	  'before-change
 	   nil
 	   t)
 	  
 (add-hook 'after-change-functions
-	  (lambda (begin end old_length) (setq floobits-after-change (buffer-substring-no-properties begin end))
-	    (process-send-string floo (json-encode floobits-change-set)))
+	  'after-change
 	  nil
 	  t)
 
