8e8cf8454cc43a875d1f6eb1377e7e74de164c9e
Matt Kaniaris <mkaniaris@gmail.com>
2013-01-11 00:59:19 -0800

    server doesn't send a version so I disabled all version checking atm

--- floobitsLineReceiver.py
+++ floobitsLineReceiver.py
@@ -11,13 +11,6 @@ class FloobitsLineReceiver(LineReceiver):
         self.agent = agent
         self.version = getattr(self.agent, "{p}_VERSION".format(p=self.dispatchPrefix.upper()))
 
-    def dispatch(self, event, version):
-        if self.version != version:
-            return None
-
-        attr = "{p}_{e}".format(p=self.dispatchPrefix, e=event)
-        return getattr(self.agent, attr, None)
-
     def lineReceived(self, line):
         req = json.loads(line)
 
@@ -25,17 +18,21 @@ class FloobitsLineReceiver(LineReceiver):
         if not event:
             raise AttributeError("no name key in req", req)
 
-        version = req.get('version')
+        version = req.get('version', None)
         if not version:
-            raise AttributeError("no name version in req", req)
+            #NOTE: fix this (have the backend send a version string)
+            # return None
+            pass
+            # raise AttributeError("no name version in req", req)
 
-        method = self.dispatch(event, version)
+        attr = "{p}_{e}".format(p=self.dispatchPrefix, e=event)
+        method = getattr(self.agent, attr, None)
         if not method:
             raise ValueError("Could not dispatch {e} for version {v}".format(e=event, v=version))
 
         return method(req)
 
     def sendLine(self, line):
-        if not isinstance(line, basestring):
+        if isinstance(line, dict):
             line = json.dumps(line)
         LineReceiver.sendLine(self, line)
