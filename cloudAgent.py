2ec9d1fab87dd4db912a91bfb5e137bdc8c25ea1
Geoff Greer <geoff@greer.fm>
2013-01-16 20:08:00 -0800

    Fix errors

--- cloudAgent.py
+++ cloudAgent.py
@@ -5,33 +5,33 @@ class CloudAgent():
         self.perms = req['perms']
         self.bufs = req['bufs']
         self.tree = req['tree']
-        #TODO: something!
+        self.sendToEditor(req)
 
-    def cloud_patch(self):
+    def cloud_patch(self, req):
         pass
 
-    def cloud_get_buf(self):
+    def cloud_get_buf(self, req):
         pass
 
-    def cloud_create_buf(self):
+    def cloud_create_buf(self, req):
         pass
 
-    def cloud_rename_buf(self):
+    def cloud_rename_buf(self, req):
         pass
 
-    def cloud_delete_buf(self):
+    def cloud_delete_buf(self, req):
         pass
 
-    def cloud_join(self):
+    def cloud_join(self, req):
         pass
 
-    def cloud_part(self):
+    def cloud_part(self, req):
         pass
 
-    def cloud_highlight(self):
+    def cloud_highlight(self, req):
         pass
 
-    def cloud_error(self):
+    def cloud_error(self, req):
         pass
 
     def cloud_disconnect(self, req, raw):
