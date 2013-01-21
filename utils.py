9bc5cc0e33c27c2cd72ea5c929146cd40bda630c
Matt Kaniaris <mkaniaris@gmail.com>
2013-01-20 19:16:16 -0800

    now handling get_buf event

--- utils.py
+++ utils.py
@@ -1,1 +1,17 @@
+import os
 
+def get_full_path(p):
+    full_path = os.path.join(G.PROJECT_PATH, p)
+    return unfuck_path(full_path)
+
+
+def unfuck_path(p):
+    return os.path.normcase(os.path.normpath(p))
+
+
+def mkdir(path):
+    try:
+        os.makedirs(path)
+    except OSError as e:
+        if e.errno != 17:
+            raise
