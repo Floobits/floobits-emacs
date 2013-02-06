937c9218492a473002516e57a02f7fae3ff492eb
Geoff Greer <geoff@greer.fm>
2013-02-06 03:37:42 -0800

    Apply incoming patches to buffers and write them to disk. Also, pass some informational events straight through to the editor

--- cloudAgent.py
+++ cloudAgent.py
@@ -1,6 +1,12 @@
+import hashlib
 import os
 
+import dmp
+import dmp_monkey
 import utils
+
+dmp_monkey.monkey_patch()
+DMP = dmp.diff_match_patch()
 
 
 class CloudAgent():
@@ -38,12 +44,44 @@ class CloudAgent():
         print('Successfully joined room %s/%s' % (self.room_owner, self.room))
 
     def cloud_patch(self, req):
-        pass
+        bufId = req['id']
+        buf = self.bufs[bufId]
+        patches = DMP.patch_fromText(req['patch'])
+        t = DMP.patch_apply(patches, buf['buf'])
+
+        clean_patch = True
+        for applied_patch in t[1]:
+            if not applied_patch:
+                clean_patch = False
+                break
+
+        if not clean_patch:
+            print('Patch not applied cleanly. Re-fetching buffer')
+            self.sendToCloud({
+                'name': 'get_buf',
+                'id': bufId,
+            })
+            return
+
+        md5_after = hashlib.md5(t[0].encode('utf-8')).hexdigest()
+        if md5_after != req['md5_after']:
+            print('md5 sum mismatch')
+
+        buf['buf'] = t[0]
+        buf['md5'] = md5_after
+
+        # For now at least save the new buffer state to disk
+        with open(buf['full_path'], 'w') as fd:
+            fd.write(buf['buf'])
+
+        # TODO: send something useful to the editor
 
     def cloud_get_buf(self, req):
         bufId = req['id']
         self.bufs[bufId] = req
         filePath = os.path.join(self.basePath, req['path'])
+        fileDir = os.path.split(filePath)[0]
+        utils.mkdir(fileDir)
         with open(filePath, 'w') as fd:
             fd.write(req['buf'])
         self.bufs[bufId]['full_path'] = filePath
@@ -53,7 +91,7 @@ class CloudAgent():
         del self.bufs[bufId]['name']
 
     def cloud_create_buf(self, req):
-        pass
+        self.cloud_get_buf(req)
 
     def cloud_rename_buf(self, req):
         pass
@@ -62,16 +100,16 @@ class CloudAgent():
         pass
 
     def cloud_join(self, req):
-        pass
+        self.sendToEditor(req)
 
     def cloud_part(self, req):
-        pass
+        self.sendToEditor(req)
 
     def cloud_highlight(self, req):
         pass
 
     def cloud_error(self, req):
-        pass
+        self.sendToEditor(req)
 
     def cloud_disconnect(self, req):
         self.cloudFactory.reconnect = False
