b66a1f14998ee8326f32cd845f877fae29159162
Geoff Greer <geoff@greer.fm>
2013-03-02 18:30:43 -0800

    Die if we have an error listening.

--- __main__.py
+++ __main__.py
@@ -1,11 +1,22 @@
 #!/usr/bin/env python
+import sys
+
 from twisted.internet.endpoints import TCP4ServerEndpoint
 from twisted.internet import reactor
 
 import editorFactory
 
+
+def onErr(err):
+	print 'AVENGE MEEEEEEEEE!'
+	sys.exit(1)
+
+
 if __name__ == "__main__":
     server = editorFactory.EditorFactory()
-    TCP4ServerEndpoint(reactor, 4567).listen(server)
+    endpoint = TCP4ServerEndpoint(reactor, 4567)
+    listener = endpoint.listen(server)
+    listener.addErrback(onErr);
+
     print('Starting up...')
     reactor.run()
