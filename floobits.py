75647aa95152813eedde9be3c4160f5bac0651ab
Geoff Greer <geoff@greer.fm>
2013-05-26 11:56:46 -0700

    Remove giant block of commented-out code that we'll never use.

--- floobits.py
+++ floobits.py
@@ -170,59 +170,6 @@ def create_room(room_name, ln_path=None, share_path=None):
     join_room(room_url, lambda x: agent.protocol.create_buf(share_path))
 
 
-# @agent_and_protocol
-# def delete_buf():
-#     name = vim.current.buffer.name
-#     agent.protocol.delete_buf(name)
-
-
-# def join_room(room_url, on_auth=None):
-#     global agent
-#     msg.debug("room url is %s" % room_url)
-
-#     try:
-#         result = utils.parse_url(room_url)
-#     except Exception as e:
-#         return msg.error(str(e))
-
-#     G.PROJECT_PATH = os.path.realpath(os.path.join(G.COLAB_DIR, result['owner'], result['room']))
-#     utils.mkdir(os.path.dirname(G.PROJECT_PATH))
-
-#     d = ''
-#     # TODO: really bad prompt here
-#     prompt = "Give me a directory to destructively dump data into (or just press enter): "
-#     if not os.path.isdir(G.PROJECT_PATH):
-#         while True:
-#             d = vim_input(prompt, d)
-#             if d == '':
-#                 utils.mkdir(G.PROJECT_PATH)
-#                 break
-#             d = os.path.realpath(os.path.expanduser(d))
-#             if not os.path.isdir(d):
-#                 prompt = '%s is not a directory. Enter an existing path (or press enter): ' % d
-#                 continue
-#             try:
-#                 os.symlink(d, G.PROJECT_PATH)
-#                 break
-#             except Exception as e:
-#                 return msg.error("Couldn't create symlink from %s to %s: %s" % (d, G.PROJECT_PATH, str(e)))
-
-#     vim.command('cd %s' % G.PROJECT_PATH)
-#     msg.debug("joining room %s" % room_url)
-
-#     stop_everything()
-#     try:
-#         start_event_loop()
-#         agent = AgentConnection(on_auth=on_auth, Protocol=Protocol, **result)
-#         # owner and room name are slugfields so this should be safe
-#         agent.connect()
-#     except Exception as e:
-#         msg.error(str(e))
-#         tb = traceback.format_exc()
-#         msg.debug(tb)
-#         stop_everything()
-
-
 # def part_room():
 #     if not agent:
 #         return msg.warn('Unable to part room: You are not joined to a room.')
