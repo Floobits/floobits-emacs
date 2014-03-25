854ba236c681982b32c075df3143de18a753b1c8
Geoff Greer <geoff@greer.fm>
2014-03-25 13:36:11 -0700

    Work around Python 3 stupidity

--- floo/editor.py
+++ floo/editor.py
@@ -61,7 +61,7 @@ def call_timeouts():
     calling_timeouts = True
     now = time.time()
     to_remove = []
-    for t, tos in timeouts.items():
+    for t, tos in timeouts.copy().items():
         if now >= t:
             for timeout in tos:
                 timeout()
