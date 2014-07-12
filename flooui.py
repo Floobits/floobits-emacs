98a38b00f1ec609a14eb2f387dd1cfb82ff5b8d7
Geoff Greer <geoff@greer.fm>
2014-07-12 14:58:25 -0700

    Squashed 'floo/common/' changes from d4c64a8..006863c
    006863c Change link editor to sign in. Add a TODO to override this message in editors other than sublime text.
    0644804 Merge bug fixes from python common.
    e1d33f1 backport delete workspace and take callback.  Fix bug in getting index (ints can be bigger than 1 digit)
    35b9a24 UnboundLocalError
    272d60b fix reactor select bug
    
    git-subtree-dir: floo/common
    git-subtree-split: 006863ceb8bc07b7659ecdff23782729c74772f3

--- flooui.py
+++ flooui.py
@@ -46,8 +46,8 @@ class FlooUI(event_emitter.EventEmitter):
 
     @utils.inlined_callbacks
     def link_account(self, context, host, cb):
-        prompt = 'No credentials found in ~/.floorc.json for %s.  Would you like to download them (opens a browser)?.' % host
-        yes = yield self.user_y_or_n, context,  prompt, "Download"
+        prompt = 'No credentials found in ~/.floorc.json for %s. Would you like to sign in? (opens a browser)' % host
+        yes = yield self.user_y_or_n, context,  prompt, 'Sign in'
         if not yes:
             return
 
@@ -78,19 +78,20 @@ class FlooUI(event_emitter.EventEmitter):
             editor.message_dialog('Thank you for installing the Floobits plugin!\n\nLet\'s set up your editor to work with Floobits.')
 
         choices = [
-            'Use an existing Floobits account',
-            'Create a new Floobits account',
+            'Sign in to Floobits',
+            'Create a Floobits account',
             'Cancel (see https://floobits.com/help/floorc)'
         ]
 
-        (choice, index) = yield self.user_select, context, 'You need a Floobits account to use Floobits! Do you want to:', choices, None
+        (choice, index) = yield self.user_select, context, 'You need an account to use Floobits! Do you want to:', choices, None
 
         if index == -1 or index == 2:
             d = utils.get_persistent_data()
             if not d.get('disable_account_creation'):
                 d['disable_account_creation'] = True
                 utils.update_persistent_data(d)
-                editor.message_dialog('''You can set up a Floobits account at any time under\n\nTools -> Floobits -> Setup''')
+                # TODO: this instruction is only useful for Sublime Text
+                editor.message_dialog('''You can set up a Floobits account at any time under\n\nTools -> Floobits -> Set up''')
             cb(None)
             return
 
@@ -409,7 +410,7 @@ class FlooUI(event_emitter.EventEmitter):
             little = ['Create workspace owned by %s' % s for s in choices]
             (owner, index) = yield self.user_select, context, 'Create workspace owned by', choices, little
 
-        if not owner or index < 0:
+        if not owner:
             return
 
         self.create_workspace(context, host, owner, workspace_name, api_args, dir_to_share)
@@ -432,9 +433,10 @@ class FlooUI(event_emitter.EventEmitter):
         cb(host)
 
     @utils.inlined_callbacks
-    def delete_workspace(self, context):
+    def delete_workspace(self, context, cb):
         host = yield self._get_host, context
         if not host:
+            cb()
             return
 
         api_url = 'https://%s/api/workspaces/can/admin' % (host)
@@ -443,16 +445,19 @@ class FlooUI(event_emitter.EventEmitter):
             r = api.api_request(host, api_url)
         except IOError as e:
             editor.error_message('Error getting workspaces can admin %s' % str_e(e))
+            cb()
             return
 
         if r.code >= 400:
             editor.error_message('Error getting workspace list: %s' % r.body)
+            cb()
             return
 
         choices = ['%s/%s' % (workspace['owner'], workspace['name']) for workspace in r.body]
         (workspace, index) = yield self.user_select, context, 'Select workpace to delete', choices, []
 
         if not workspace:
+            cb()
             return
 
         if G.EXPERT_MODE:
@@ -461,6 +466,7 @@ class FlooUI(event_emitter.EventEmitter):
             yes = yield self.user_y_or_n, context, 'Really delete %s?' % workspace, 'Yes'
 
         if not yes:
+            cb()
             return
 
         workspace = r.body[index]
@@ -469,4 +475,4 @@ class FlooUI(event_emitter.EventEmitter):
             api.delete_workspace(host, workspace['owner'], workspace['name'])
         except IOError as e:
             editor.error_message('Error deleting workspace' % str_e(e))
-            return
+        cb()
