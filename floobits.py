fcacbf0ccad090c5b72de676ede08d2009ed0619
Geoff Greer <geoff@greer.fm>
2015-01-04 19:04:27 -0800

    Parse arguments. Add port option for debugging.

--- floobits.py
+++ floobits.py
@@ -2,6 +2,7 @@
 # coding: utf-8
 import os
 import sys
+import optparse
 from floo import emacs_handler
 from floo.common import migrations
 from floo.common import reactor
@@ -17,6 +18,16 @@ def cb(port):
 def main():
     G.__VERSION__ = '0.11'
     G.__PLUGIN_VERSION__ = '1.5.15'
+
+    parser = optparse.OptionParser(usage='%prog [options]')
+    parser.add_option("-p", "--port",
+                      dest="port",
+                      default=0,
+                      help="The port to listen on. Useful for debugging.")
+
+    options, args = parser.parse_args()
+    port = int(options.port)
+
     utils.reload_settings()
 
     if not os.path.exists(G.FLOORC_JSON_PATH):
@@ -33,7 +44,7 @@ def main():
 
     emacs = emacs_handler.EmacsHandler()
     G.emacs = emacs
-    _, port = reactor.reactor.listen(emacs)
+    _, port = reactor.reactor.listen(emacs, port=port)
     utils.set_timeout(cb, 100, port)
     reactor.reactor.block()
 
